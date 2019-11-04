1972a378866deef878e04afeeb8041a83c444386
Edyta2801 <edyta.szarowska@interia.pl>
2019-11-04 10:27:36 +0100

    add List component

---
+++ src/components/List/List.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class List extends React.Component {
+    render() {
+        return (
+            <section className={styles.component}>
+                <h2 className={styles.subtitle}>List to do</h2>
+            </section>
+        );
+    }
+}
+export default List;
