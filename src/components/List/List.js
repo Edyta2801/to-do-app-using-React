d14eff8d493aae8811543527fd1bd9f3493e95cb
Edyta2801 <edyta.szarowska@interia.pl>
2019-11-04 21:44:35 +0100

    fix bugs

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
-import PropTypes from 'prop-types';
 
 class List extends React.Component {
-    static propTypes = {
-        title: PropTypes.node.isRequired,
-        children: PropTypes.node,
-    }
-    render() {
-        return (
-            <section className={styles.component}>
-                <Hero titleText={this.props.title} />
-                <div className={styles.description}>
-                    {this.props.children}
-                </div>
-            </section>
-        );
-    }
+  render() {
+    return (
+      <section className={styles.component}>
+        <Hero />
+      </section>
+    );
+  }
 }
+
 export default List;
