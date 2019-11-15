b83140b0c810007de99478078bfb57040216adfa
Edyta2801 <edyta.szarowska@interia.pl>
2019-11-15 10:27:47 +0100

    change defaultProps and props.chilren in coponent List

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,22 +3,24 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import Column from '../Column/Column.js';
 import PropTypes from 'prop-types';
+import {settings} from '../../data/dataStore';
 
 
 class List extends React.Component {
   static propTypes = {
     title: PropTypes.node.isRequired,
-    children: PropTypes.node.isRequired,
+    description:PropTypes.node,
+    columns:PropTypes.array,
     image:PropTypes.string.isRequired,
   }
   static defaultProps = {
-    children: <p>I can do all the things!!!</p>
+    description: settings.defaultListDescription
   }
   render() {
     return (
       <section className={styles.component}>
       <Hero titleText={this.props.title} imageURL={this.props.image}/>
-        <div className={styles.description}>{this.props.children}</div>
+        <div className={styles.description}>{this.props.description}</div>
         <div className={styles.columns}>
           <Column title="Animals" />
           <Column title="Plants" />
