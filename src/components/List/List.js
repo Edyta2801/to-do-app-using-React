cbc2c01ecda8efe0685991ea3dcb4bb1419bd52c
Edyta2801 <edyta.szarowska@interia.pl>
2019-11-15 10:33:45 +0100

    parse this.props.description in List component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,6 +4,7 @@ import Hero from '../Hero/Hero.js';
 import Column from '../Column/Column.js';
 import PropTypes from 'prop-types';
 import {settings} from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
 
 
 class List extends React.Component {
@@ -20,7 +21,7 @@ class List extends React.Component {
     return (
       <section className={styles.component}>
       <Hero titleText={this.props.title} imageURL={this.props.image}/>
-        <div className={styles.description}>{this.props.description}</div>
+        <div className={styles.description}>{ReactHtmlParser(this.props.description)}</div>
         <div className={styles.columns}>
           <Column title="Animals" />
           <Column title="Plants" />
