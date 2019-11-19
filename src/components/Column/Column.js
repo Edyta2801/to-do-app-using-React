bee43d67a9ac1bccd639cb14d302ef442fe591dc
Edyta2801 <edyta.szarowska@interia.pl>
2019-11-19 12:25:46 +0100

    modify Column to add new Card

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,16 +1,45 @@
 import React from 'react';
 import styles from "./Column.scss";
 import PropTypes from 'prop-types';
+import { settings } from '../../data/dataStore.js';
+import Creator from '../Creator/Creator.js';
 
 class Column extends React.Component {
+  state={
+    cards:this.props.cards||[],
+  }
   static propTypes = {
     title: PropTypes.node.isRequired,
+    cards:PropTypes.array,
 
   };
+
+addCard(title){
+  this.setState(state=>({
+    cards:[
+      ...state.cards,
+      {
+        key: state.cards.length ? state.cards[state.cards.length - 1].key + 1 : 0,
+        title,
+        icon: 'list-alt',
+        cards: []
+      }
+    ]
+  }))
+}
+
   render() {
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>{this.props.title}</h3>
+        <div className={styles.cards}>
+          {this.state.cards.map(({ key, ...cardProps }) => (
+            <Column key={key}{...cardProps} />
+          ))}
+        </div>
+        <div className={styles.creator}>
+          <Creator text={settings.cardCreatorText} action={title=>this.addCard(title)} />
+        </div>
       </section>
     );
   }
