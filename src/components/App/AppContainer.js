019ce145f8fc5df0b9b4a0b076594a9f92399d6c
Edyta2801 <edyta.szarowska@interia.pl>
2019-11-24 22:37:48 +0100

    give AppComponents contents of subtitle

--- src/components/App/AppContainer.js
+++ src/components/App/AppContainer.js
@@ -1,8 +1,9 @@
 import { connect } from 'react-redux';
-import App from './App';
+import App from './App.js';
 
 const mapStateToProps = state => ({
   title: state.app.title,
+  subtitle: state.app.subtitle,
 });
 
 export default connect(mapStateToProps)(App);
