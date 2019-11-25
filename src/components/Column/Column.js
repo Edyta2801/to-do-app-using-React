bd9bbde4c0b2d6170946674ea6630848f85ae5b5
Edyta2801 <edyta.szarowska@interia.pl>
2019-11-25 11:57:45 +0100

    dispaly cards in columns

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Column.scss';
 import Icon from '../Icon/Icon.js';
 // import Creator from '../Creator/Creator.js';
-// import Card from '../Card/Card.js';
+import Card from '../Card/Card.js';
 import PropTypes from 'prop-types';
 // import { settings } from '../../data/dataStore.js';
 
@@ -33,19 +33,19 @@ class Column extends React.Component {
   // }
 
   render() {
-    const {title, icon}=this.props;
+    const {title, icon, cards}=this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>{title}</h3>
         <span className={styles.icon}>
           <Icon name={icon} />
         </span>
-        {/* <div className={styles.cards}>
-          {this.state.cards.map(({ key, ...cardProps }) => (
-            <Card key={key}{...cardProps} />
+        <div className={styles.cards}>
+          {cards.map(cardData => (
+            <Card key={cardData.id}{...cardData} />
           ))}
         </div>
-        <div className={styles.creator}>
+        {/* <div className={styles.creator}>
           <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
         </div> */}
       </section>
