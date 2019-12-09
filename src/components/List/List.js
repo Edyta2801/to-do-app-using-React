55ecbf3599c1d2c6431f0fc296be79c535880105
Edyta2801 <edyta.szarowska@interia.pl>
2019-12-09 21:30:55 +0100

    add Container Component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -6,7 +6,7 @@ import Creator from '../Creator/Creator.js';
 import PropTypes from 'prop-types';
 import { settings } from '../../data/dataStore.js';
 import ReactHtmlParser from 'react-html-parser';
-
+import Container from '../Container/Container';
 
 class List extends React.Component {
 
@@ -18,7 +18,7 @@ class List extends React.Component {
     description: PropTypes.node,
     columns: PropTypes.array,
     image: PropTypes.string.isRequired,
-    addColumn:PropTypes.func,
+    addColumn: PropTypes.func,
   }
   static defaultProps = {
     description: settings.defaultListDescription,
@@ -38,19 +38,21 @@ class List extends React.Component {
   //   }));
   // }
   render() {
-    const {title, image, description, columns, addColumn}=this.props;
+    const { title, image, description, columns, addColumn } = this.props;
     return (
       <section className={styles.component}>
-        <Hero titleText={title} imageURL={image} />
-        <div className={styles.description}>{ReactHtmlParser(description)}</div>
-        <div className={styles.columns}>
-          {columns.map(columnData=> (
-            <Column key={columnData.id}{...columnData} />
-          ))}
-        </div>
-        <div className={styles.creator}>
-          <Creator text={settings.columnCreatorText} action={addColumn} />
-        </div>
+        <Container>
+          <Hero titleText={title} imageURL={image} />
+          <div className={styles.description}>{ReactHtmlParser(description)}</div>
+          <div className={styles.columns}>
+            {columns.map(columnData => (
+              <Column key={columnData.id}{...columnData} />
+            ))}
+          </div>
+          <div className={styles.creator}>
+            <Creator text={settings.columnCreatorText} action={addColumn} />
+          </div>
+        </Container>
       </section>
     );
   }
