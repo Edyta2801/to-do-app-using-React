c3b8eed3fe9ea40fc0a75906b1d73de10885fbb4
Edyta2801 <edyta.szarowska@interia.pl>
2019-12-11 12:38:25 +0100

    fix bug and create smooth animation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from '../Home/HomeContainer';
 import Info from '../Info/Info';
 import FAQ from '../FAQ/FAQ';
+import style from './App.scss';
 import { BrowserRouter,  Route } from 'react-router-dom';
 import MainLayout from '../MainLayout/MainLayout';
 import { AnimatedSwitch } from 'react-router-transition';
@@ -13,7 +14,7 @@ const App = () => (
         atEnter={{ opacity: 0 }}
         atLeave={{ opacity: 0 }}
         atActive={{ opacity: 1 }}
-        className="switch-wrapper"
+        className={style.switchWrapper}
       >
         <Route exact path='/' component={Home} />
         <Route exact path='/info' component={Info} />
