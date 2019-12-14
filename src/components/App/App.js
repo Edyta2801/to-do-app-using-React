d183d7180da31524aa026ce67ba1b2e843ac5849
Edyta2801 <edyta.szarowska@interia.pl>
2019-12-14 09:52:13 +0100

    change mapStateToProps

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,9 +3,10 @@ import Home from '../Home/HomeContainer';
 import Info from '../Info/Info';
 import FAQ from '../FAQ/FAQ';
 import style from './App.scss';
-import { BrowserRouter,  Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import MainLayout from '../MainLayout/MainLayout';
 import { AnimatedSwitch } from 'react-router-transition';
+import List from '../List/ListContainer';
 
 const App = () => (
   <BrowserRouter>
@@ -19,6 +20,7 @@ const App = () => (
         <Route exact path='/' component={Home} />
         <Route exact path='/info' component={Info} />
         <Route exact path='/FAQ' component={FAQ} />
+        <Route exact path='/list/:id' component={List} />
       </AnimatedSwitch>
     </MainLayout>
   </BrowserRouter>
